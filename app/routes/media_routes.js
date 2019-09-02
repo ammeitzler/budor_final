8999e252ce1ac4afb1279ac61bdb27a113420baa
Angeline <ammeitzler@gmail.com>
2019-09-02 01:37:13 -0400

    seperate db

--- app/routes/media_routes.js
+++ app/routes/media_routes.js
@@ -3,7 +3,7 @@ module.exports = function(app, db) {
   var ObjectID = require('mongodb').ObjectID;
 
   app.get("/media", function(req, res, next) {
-    db.collection("rasp_data", function(err, result){
+    db.collection("rasp2_data", function(err, result){
         result.find({}).sort({_id: -1}).limit(10).toArray(function(err, data){
           res.json(data);
       })
@@ -13,7 +13,7 @@ module.exports = function(app, db) {
   app.delete('/media/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
-    db.collection('rasp_data').remove(details, (err, item) => {
+    db.collection('rasp2_data').remove(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -26,7 +26,7 @@ module.exports = function(app, db) {
     console.log(req.body)
     const media_data = { values: req.body.values };
 
-    db.collection('rasp_data').insert(media_data, (err, result) => {
+    db.collection('rasp2_data').insert(media_data, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
--- app/routes/signal_routes.js
+++ app/routes/signal_routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, db) {
   //for turning on and off
 
   app.get("/signal", function(req, res, next) {
-    db.collection("onoff_data", function(err, result){
+    db.collection("onoff2_data", function(err, result){
         result.find({}).sort({_id: -1}).limit(10).toArray(function(err, data){
           res.json(data);
       })
@@ -14,7 +14,7 @@ module.exports = function(app, db) {
   app.delete('/signal/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
-    db.collection('onoff_data').remove(details, (err, item) => {
+    db.collection('onoff2_data').remove(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -27,7 +27,7 @@ module.exports = function(app, db) {
     console.log(req.body)
     const signal_data = { values: req.body.values };
 
-    db.collection('onoff_data').insert(signal_data, (err, result) => {
+    db.collection('onoff2_data').insert(signal_data, (err, result) => {
       if (err) { 
         res.send({ 'error': 'An error has occurred' }); 
       } else {
